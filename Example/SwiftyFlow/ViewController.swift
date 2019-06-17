6e3aba95f5153880c0da7eff098529310aeeadc1
Felipe Garcia <felipe.garcia@nl.abnamro.com>
2019-06-17 09:34:02 +0200

    Cleaning code and just small adjust the visual for the sample

--- Example/SwiftyFlow/ViewController.swift
+++ Example/SwiftyFlow/ViewController.swift
@@ -11,27 +11,12 @@ import SwiftyFlow
 
 class ViewController: UIViewController {
     
-    // You need to make sure that you initialize this, at least for now as a pock
-    var flowManager: FlowManager!
-    var flowManagerFromScratch: FlowManager?
-    
     @IBOutlet weak var pushButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Swifty Flow"
-        
-        //Setup my flow for when using Storyboard
-        self.flowManager = createMyStoryboardFlowManager()
-    }
-    
-    private func createMyStoryboardFlowManager() -> FlowManager {
-        let flowManager = FlowManager(navigation: self.navigationController!, container: ContainerFlowStack())
-
-        // Setup stack navigation - Conceptual Idea
-        ContainerView().setupStackNavigation(using: flowManager.container()!)
-        return flowManager
     }
     
     // MARK: - Automatically Navigation Flow Using Storyboard
@@ -121,44 +106,5 @@ class ViewController: UIViewController {
                                                         debugPrint("finished")
         })
         flowManagerUsingNibWithModalFlow?.start()
-    }
-    
-    // MARK: - Navigation when Storyboard Views
-    @IBAction func storyboardNavigationAction() {
-        self.flowManager?.goNext(screen: FirstViewController.self, resolve: .storyboard("Main"))
-        
-        self.flowManager.dismissedFlowWith { parameter in
-            debugPrint("Finished passing this parameter \(parameter) that has the type: \(type(of: parameter))")
-        }
-    }
-    
-    // MARK: - Navigation when Nib Views
-    @IBAction func nibNavigationAction() {
-        self.flowManager?.goNext(screen: FirstViewController.self, resolve: .nib)
-    }
-    
-    // MARK: - Navigation with custom Navigation Controller
-    @IBAction func newOneFromContainerAction() {
-        // With this you can have you instance and handle dismiss and do what ever you want
-        // Using this format as soon you finish load your navigation flow manager you will
-        // present you navigation controller flow automatically
-        flowManagerFromScratch = createNavigationFromScratch()
-        
-        flowManagerFromScratch?.start()
-    }
-    
-    // Helper To Create the container
-    func createNavigationFromScratch() -> FlowManager? {
-        let navigationStack = ContainerFlowStack()
-        ContainerView().setupStackNavigation(using: navigationStack)
-
-        // As soon finish instantiate you will show you new navigation flow
-        return FlowManager(root: FirstViewController.self, container: navigationStack,
-           dismissed: {
-            // Finished dismissing navigation flow completely
-            debugPrint("Finished dismiss navigation controller using dismiss()")
-        }).dismissedFlowWith(parameter: { parameter in
-            debugPrint("Finished passing this parameter \(parameter) that has the type: \(type(of: parameter))")
-        })
     }
 }
